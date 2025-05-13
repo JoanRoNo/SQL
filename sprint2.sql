5075b3ad514e21a6d8e299f9faf232d40880c40c
JoanRoNo <joanrono4@gmail.com>
2025-05-13 13:57:49 +0200

    Add files via upload

--- sprint2.sql
+++ sprint2.sql
@@ -34,3 +34,101 @@ WHERE
 	FROM company
 	WHERE country = "Germany");
 
+# Llista les empreses que han realitzat transaccions per un amount superior a la mitjana de totes les transaccions.
+SELECT company_name
+FROM company
+WHERE id IN (
+    SELECT company_id
+    FROM transaction
+    WHERE amount > (
+        SELECT AVG(amount)
+        FROM transaction
+    )
+);
+
+
+# Eliminaran del sistema les empreses que no tenen transaccions registrades, entrega el llistat d'aquestes empreses.
+SELECT company_name
+FROM company
+WHERE id NOT IN (
+	SELECT company_id
+    FROM transaction
+    WHERE company_id IS NOT NULL);
+    
+# Nivell 2 
+# Identifica els cinc dies que es va generar la quantitat més gran d'ingressos a l'empresa per vendes. Mostra la data de cada transacció juntament amb el total de les vendes.
+SELECT DATE(timestamp) as dia, sum(amount) as ingresos 
+FROM transaction
+GROUP BY dia
+ORDER BY ingresos desc
+LIMIT 5;
+
+# Quina és la mitjana de vendes per país? Presenta els resultats ordenats de major a menor mitjà.
+select company.country, count(*) as ventas
+from transaction
+join company on transaction.company_id = company.id
+group by company.country
+order by ventas desc;
+
+# En la teva empresa, es planteja un nou projecte per a llançar algunes campanyes publicitàries per a fer competència 
+# a la companyia "Non Institute". Per a això, et demanen la llista de totes les transaccions realitzades per empreses
+# que estan situades en el mateix país que aquesta companyia.
+
+# fet amb joins i subqueries
+select * 
+from transaction
+join company on 
+transaction.company_id = company.id
+where company.country in (
+	select country
+	from company
+	where company_name = "Non Institute");
+    
+# fet amb subqueries:
+
+SELECT *
+FROM transaction
+WHERE company_id IN (
+	SELECT id
+    FROM company
+    WHERE country = (
+		SELECT country
+        FROM company
+        WHERE company_name = "Non Institute")
+        );
+
+# Nivell 3
+# Presenta el nom, telèfon, país, data i amount, d'aquelles empreses que van realitzar transaccions amb un valor comprès entre 100 i 200 euros
+# i en alguna d'aquestes dates: 29 d'abril del 2021, 20 de juliol del 2021 i 13 de març del 2022. 
+# Ordena els resultats de major a menor quantitat.
+
+SELECT c.company_name, c.phone, c.country, date(t.timestamp) as fecha, t.amount
+FROM company c
+JOIN transaction t on c.id=t.company_id
+WHERE amount between 100 and 200
+HAVING fecha = "2021-04-29"
+or fecha = "2021-07-20"
+or fecha = "2022-03-13"
+ORDER BY amount desc;
+
+# Necessitem optimitzar l'assignació dels recursos i dependrà de la capacitat operativa que es requereixi, 
+# per la qual cosa et demanen la informació sobre la quantitat de transaccions que realitzen les empreses, 
+# però el departament de recursos humans és exigent i vol un llistat de les empreses on especifiquis si tenen més de 4 transaccions o menys.
+
+SELECT company_id, count(*) as num_transacciones, IF (count(*) >= 4, "mayor/igual a 4", "menor a 4") as transacciones
+FROM transaction
+GROUP BY company_id
+ORDER BY num_transacciones desc; 
+
+     
+
+
+
+
+
+
+
+
+	
+
+
